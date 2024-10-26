5a02f8128102fc199db260ed738d9a70b8db2c0f
Cam <cameron.mcintyre@csuglobal.edu>
2024-10-26 14:48:11 -0600

    Added CSC320 Module 3 CTA project

---
+++ CSC320_projects/CSC320_Module3_CTA.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class CSC320_Module3_CTA {
+    
+    public static void main(String[] args){
+        
+        Scanner getinfo = new Scanner(System.in);
+                
+        int income = 0;
+        int incomecase = 0;
+
+        System.out.println("Welcome of the tax calculator! ");
+        System.out.println("Please enter your weekly income:");
+
+        income = getinfo.nextInt();
+        
+        if (income < 0){
+            incomecase = 999;
+        }
+        else {
+            incomecase = income/500;
+        }
+
+        switch (incomecase){
+
+            case 0:
+                System.out.println("Your tax rate is 10%");
+                break;
+            case 1:
+            case 2:
+                System.out.println("Your tax rate is 15%");
+                break;
+            case 3:
+            case 4:
+                System.out.println("Your tax rate is 20%");
+                break;
+            case 999:
+                System.out.println("You had a horrible week and made negative money.  Your tax rate is 0%");
+                break;
+            default:
+                System.out.println("Your tax rate is 30%");
+                break;
+        }   
+    }
+}
--- gitTestProgram.java
+++ gitTestProgram.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class gitTestProgram {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a value: ");
-
-        String userInput = scanner.nextline();
-
-        System.out.println("You entered: " + userInput);
-
-        scanner.close();
+        int it = 1;
+        
+        if (it == 1){
+            System.out.println("Hi");
+            System.out.println("Hi");
+            System.out.println("Hi");
+        }
+            
     }
 }
