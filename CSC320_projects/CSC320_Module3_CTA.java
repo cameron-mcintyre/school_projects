fd3a0c7a52610eac57df71958d2d684a0f7f90bb
Cam <cameron.mcintyre@csuglobal.edu>
2024-11-17 13:23:40 -0700

    uploaded module 6 info and a corrected module 3 CTA assignment

--- CSC320_projects/CSC320_Module3_CTA.java
+++ CSC320_projects/CSC320_Module3_CTA.java
@@ -5,43 +5,40 @@ public class CSC320_Module3_CTA {
     public static void main(String[] args){
         
         Scanner getinfo = new Scanner(System.in);
-                
-        int income = 0;
-        int incomecase = 0;
+        double taxes = 0;        
+        double income = 0;
+        boolean keepgoing = true;
+        String keepgoingchoice;
+
+        while (keepgoing == true) {
 
         System.out.println("Welcome of the tax calculator! ");
         System.out.println("Please enter your weekly income:");
-
-        income = getinfo.nextInt();
+        income = getinfo.nextDouble();
         
-        if (income < 0){
-            incomecase = 999;
-        }
-        else {
-            incomecase = income/500;
+            if (income < 0){
+                System.out.println("You have no taxes! Taxes = " + taxes);
+            } else if (income < 500.00){
+                taxes = income * 0.1;
+                System.out.println("You are taxed at 10%.  Taxes = " + taxes);
+            } else if (income < 1500.00){
+                taxes = income * 0.15;
+                System.out.println("You are taxed at 15%.  Taxes = " + taxes); 
+            } else if (income < 2500){
+                taxes = income * 0.2;
+                System.out.println("You are taxed at 20%.  Taxes = " + taxes);
+            } else {
+                taxes = income * 0.3;
+                System.out.println("You are taxed at 30%.  Taxes = " + taxes);
+            }
+
+            System.out.println("Do you want to go again? Enter 'N' to stop: ");
+            keepgoingchoice = getinfo.next();
+            if (keepgoingchoice.equals("N")){
+                keepgoing = false;
+            }
         }
 
-        switch (incomecase){
-
-            case 0:
-                System.out.println("Your tax rate is 10%");
-                break;
-            case 1:
-            case 2:
-                System.out.println("Your tax rate is 15%");
-                break;
-            case 3:
-            case 4:
-                System.out.println("Your tax rate is 20%");
-                break;
-            case 999:
-                System.out.println("You had a horrible week and made negative money.  Your tax rate is 0%");
-                break;
-            default:
-                System.out.println("Your tax rate is 30%");
-                break;
-        }
-           
         getinfo.close();
     }
 }
--- CSC320_projects/CSC320_Module5_disc.java
+++ CSC320_projects/CSC320_Module5_disc.java
@@ -4,19 +4,32 @@ public class CSC320_Module5_disc {
 
     public static void main(String[] args){
 
-        Scanner scnr = new Scanner(System.in);
         String[] trainCars = new String[10];
+
+        trainCars = getTrain(trainCars);
+        printTrain(trainCars);        
+    }
+
+    public static String[] getTrain(String[] trainCars){
+        
+        Scanner scnr = new Scanner(System.in);
         int i;
-        //Build the locomotive.
+
         System.out.println("Your train has one locomotive and ten cars!  Enter your locomotive: ");
         trainCars[0] = scnr.next();
-        //Now add all the cars.
+
         for (i = 1; i < trainCars.length; ++i){
             System.out.println("Enter the type of train car");
             trainCars[i] = scnr.next();
             System.out.println((trainCars.length - i - 1) + " cars left!");
         }
-        //Print out the train.
+        scnr.close();
+        return trainCars;
+    }
+
+    public static void printTrain(String[] trainCars){
+        int i = 0;
+
         System.out.println("");
         System.out.println("Your train has the following cars:");
 
@@ -24,8 +37,7 @@ public class CSC320_Module5_disc {
             System.out.print(trainCars[i] + "-");
             
         }
-        //Don't forget a caboose!  I know trains don't use them anymore, I don't care.
+        
         System.out.print("Caboose");
     }
-    
 }
